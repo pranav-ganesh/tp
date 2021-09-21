f8bd91cb268173c3e2c6f712ff22b5e9fefcc917
houtenteo <houtenteo@gmail.com>
2021-09-21 15:11:19 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -96,8 +96,8 @@ public class EditCommand extends Command {
         Name updatedName = editPersonDescriptor.getName().orElse(personToEdit.getName());
         Phone updatedPhone = editPersonDescriptor.getPhone().orElse(personToEdit.getPhone());
         Email updatedEmail = editPersonDescriptor.getEmail().orElse(personToEdit.getEmail());
-//        Address updatedAddress = editPersonDescriptor.getAddress().orElse(personToEdit.getAddress());
-//        Set<Tag> updatedTags = editPersonDescriptor.getTags().orElse(personToEdit.getTags());
+        //Address updatedAddress = editPersonDescriptor.getAddress().orElse(personToEdit.getAddress());
+        //Set<Tag> updatedTags = editPersonDescriptor.getTags().orElse(personToEdit.getTags());
 
         //return new Person(updatedName, updatedPhone, updatedEmail, updatedAddress, updatedTags);
         return new Person(updatedName, updatedPhone, updatedEmail, false);
--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -1,16 +1,12 @@
 package seedu.address.storage;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
@@ -34,18 +30,18 @@ class JsonAdaptedPerson {
     /**
      * Constructs a {@code JsonAdaptedPerson} with the given person details.
      */
-//    @JsonCreator
-//    public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
-//            @JsonProperty("email") String email, @JsonProperty("address") String address,
-//            @JsonProperty("tagged") List<JsonAdaptedTag> tagged) {
-//        this.name = name;
-//        this.phone = phone;
-//        this.email = email;
-//        this.address = address;
-//        if (tagged != null) {
-//            this.tagged.addAll(tagged);
-//        }
-//    }
+    //    @JsonCreator
+    //    public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
+    //            @JsonProperty("email") String email, @JsonProperty("address") String address,
+    //            @JsonProperty("tagged") List<JsonAdaptedTag> tagged) {
+    //        this.name = name;
+    //        this.phone = phone;
+    //        this.email = email;
+    //        this.address = address;
+    //        if (tagged != null) {
+    //            this.tagged.addAll(tagged);
+    //        }
+    //    }
     @JsonCreator
     public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
                              @JsonProperty("email") String email, @JsonProperty("done") boolean isDone) {
@@ -58,15 +54,15 @@ class JsonAdaptedPerson {
     /**
      * Converts a given {@code Person} into this class for Jackson use.
      */
-//    public JsonAdaptedPerson(Person source) {
-//        name = source.getName().fullName;
-//        phone = source.getPhone().value;
-//        email = source.getEmail().value;
-//        address = source.getAddress().value;
-//        tagged.addAll(source.getTags().stream()
-//                .map(JsonAdaptedTag::new)
-//                .collect(Collectors.toList()));
-//    }
+    //    public JsonAdaptedPerson(Person source) {
+    //        name = source.getName().fullName;
+    //        phone = source.getPhone().value;
+    //        email = source.getEmail().value;
+    //        address = source.getAddress().value;
+    //        tagged.addAll(source.getTags().stream()
+    //                .map(JsonAdaptedTag::new)
+    //                .collect(Collectors.toList()));
+    //    }
 
     public JsonAdaptedPerson(Person source) {
         name = source.getName().fullName;
@@ -82,9 +78,9 @@ class JsonAdaptedPerson {
      */
     public Person toModelType() throws IllegalValueException {
         final List<Tag> personTags = new ArrayList<>();
-//        for (JsonAdaptedTag tag : tagged) {
-//            personTags.add(tag.toModelType());
-//        }
+        //        for (JsonAdaptedTag tag : tagged) {
+        //            personTags.add(tag.toModelType());
+        //        }
 
         if (name == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName()));
@@ -110,16 +106,17 @@ class JsonAdaptedPerson {
         }
         final Email modelEmail = new Email(email);
 
-//        if (address == null) {
-//            throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Address.class.getSimpleName()));
-//        }
-//        if (!Address.isValidAddress(address)) {
-//            throw new IllegalValueException(Address.MESSAGE_CONSTRAINTS);
-//        }
-//        final Address modelAddress = new Address(address);
-//
-//        final Set<Tag> modelTags = new HashSet<>(personTags);
-//        return new Person(modelName, modelPhone, modelEmail, modelAddress, modelTags);
+        //        if (address == null) {
+        //            throw new IllegalValueException(
+        //              String.format(MISSING_FIELD_MESSAGE_FORMAT, Address.class.getSimpleName()));
+        //        }
+        //        if (!Address.isValidAddress(address)) {
+        //            throw new IllegalValueException(Address.MESSAGE_CONSTRAINTS);
+        //        }
+        //        final Address modelAddress = new Address(address);
+        //
+        //        final Set<Tag> modelTags = new HashSet<>(personTags);
+        //        return new Person(modelName, modelPhone, modelEmail, modelAddress, modelTags);
         return new Person(modelName, modelPhone, modelEmail, isDone);
     }
 
