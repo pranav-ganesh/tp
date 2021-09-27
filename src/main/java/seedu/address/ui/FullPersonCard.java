f57232ab2356bc9e1a49425181cc86e3464bf938
houtenteo <houtenteo@gmail.com>
2021-09-27 15:54:06 +0800

    Add error handling
    In the event that the list is empty, display an empty FullPersonCard.

--- src/main/java/seedu/address/ui/FullPersonCard.java
+++ src/main/java/seedu/address/ui/FullPersonCard.java
@@ -15,7 +15,7 @@ public class FullPersonCard extends UiPart<Region> {
     private static final String FXML = "FullPersonCard.fxml";
     private final Logger logger = LogsCenter.getLogger(FullPersonCard.class);
 
-    public final Person person;
+    public Person person;
 
     @FXML
     private HBox cardPane;
@@ -46,30 +46,55 @@ public class FullPersonCard extends UiPart<Region> {
     private final String AGE_LABEL = "Age: ";
     private final String INTERESTS_LABEL = "Interests: ";
     private final String DEFAULT_FIELD = "N.A";
+    private final String NO_PERSON_TO_DISPLAY = "No person to display";
 
-
+    /**
+     * Constructor for the FullPersonCard
+     * @param persons The current list displayed
+     * @param displayedIndex The index of the person to display on the card
+     * @param windowWidth The size of the card
+     */
     public FullPersonCard(List<Person> persons, int displayedIndex, double windowWidth) {
         super(FXML);
 
         cardPane.setPrefWidth(windowWidth / 2);
+        try {
+            this.person = persons.get(displayedIndex - 1);
+            id.setText(displayedIndex + ". ");
+            name.setText(person.getName().fullName);
+            phone.setText(PHONE_LABEL + person.getPhone().value);
+            email.setText(EMAIL_LABEL + person.getEmail().value);
 
-        this.person = persons.get(displayedIndex - 1);
-        id.setText(displayedIndex + ". ");
-        name.setText(person.getName().fullName);
-        phone.setText(PHONE_LABEL + person.getPhone().value);
-        email.setText(EMAIL_LABEL + person.getEmail().value);
+            if (person.getIsDone().value) {
+                isDone.setText(DONE_LABEL + "True");
+            } else {
+                isDone.setText(DONE_LABEL + "False");
+            }
 
-        if (person.getIsDone().value) {
-            isDone.setText(DONE_LABEL + "True");
-        } else {
-            isDone.setText(DONE_LABEL + "False");
+            address.setText(ADDRESS_LABEL + DEFAULT_FIELD);
+            gender.setText(GENDER_LABEL + DEFAULT_FIELD);
+            age.setText(AGE_LABEL + DEFAULT_FIELD);
+            interests.setText(INTERESTS_LABEL + DEFAULT_FIELD);
+        } catch (IndexOutOfBoundsException e) {
+            createEmptyCard();
         }
+    }
 
+    /**
+     * In the event that the list is empty, an empty card will be shown.
+     */
+    private void createEmptyCard() {
+        this.person = null;
+        id.setText(NO_PERSON_TO_DISPLAY);
+        name.setText("");
+        phone.setText(PHONE_LABEL + DEFAULT_FIELD);
+        email.setText(EMAIL_LABEL + DEFAULT_FIELD);
+        isDone.setText(DONE_LABEL + DEFAULT_FIELD);
+        address.setText(ADDRESS_LABEL + DEFAULT_FIELD);
         address.setText(ADDRESS_LABEL + DEFAULT_FIELD);
         gender.setText(GENDER_LABEL + DEFAULT_FIELD);
         age.setText(AGE_LABEL + DEFAULT_FIELD);
         interests.setText(INTERESTS_LABEL + DEFAULT_FIELD);
-
     }
 
 }
