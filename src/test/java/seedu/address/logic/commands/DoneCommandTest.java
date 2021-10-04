9d645ef3e273f97f41ddecbbeb15d17ed8b1891a
pranav_ganesh <e0661424@u.nus.edu>
2021-10-04 10:04:32 +0800

    Fix more code style errors

--- src/test/java/seedu/address/logic/commands/DoneCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DoneCommandTest.java
@@ -1,7 +1,7 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
