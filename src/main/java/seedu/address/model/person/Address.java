420a06b430a0d66f18b8b466dd1d61bd85c5c555
houtenteo <houtenteo@gmail.com>
2021-10-05 21:39:22 +0800

    Update message constraints

--- src/main/java/seedu/address/model/person/Address.java
+++ src/main/java/seedu/address/model/person/Address.java
@@ -8,7 +8,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
  */
 public class Address {
 
-    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank";
+    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, but should not be an empty string";
     public static final String NO_ADDRESS = "N.A";
 
     /*
