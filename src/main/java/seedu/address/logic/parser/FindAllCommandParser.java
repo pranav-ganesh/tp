3b9b521f146f340e14feb1284d93eb61bca340d8
houtenteo <houtenteo@gmail.com>
2021-11-01 18:37:21 +0800

    Refactor find command parser methods into ParserUtil class

--- src/main/java/seedu/address/logic/parser/FindAllCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindAllCommandParser.java
@@ -49,7 +49,7 @@ public class FindAllCommandParser implements Parser<FindAllCommand> {
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_DONE,
                         PREFIX_ADDRESS, PREFIX_GENDER, PREFIX_AGE, PREFIX_INTEREST);
 
-        if (!arePrefixesPresent(argMultimap) || !argMultimap.getPreamble().isEmpty()) {
+        if (!ParserUtil.arePrefixesPresent(argMultimap) || !argMultimap.getPreamble().isEmpty()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindAllCommand.MESSAGE_USAGE));
         }
 
@@ -65,45 +65,45 @@ public class FindAllCommandParser implements Parser<FindAllCommand> {
         ArrayList<Predicate<Person>> predicates = new ArrayList<>();
 
         if (nameKeywords != null) {
-            checkEmptyString(nameKeywords, PREFIX_NAME);
-            NameContainsKeywordsPredicate namePredicate = getNamePredicate(nameKeywords);
+            ParserUtil.checkEmptyString(nameKeywords, PREFIX_NAME);
+            NameContainsKeywordsPredicate namePredicate = ParserUtil.getNamePredicate(nameKeywords, true);
             predicates.add(namePredicate);
         }
         if (phoneNumbers != null) {
-            checkEmptyString(phoneNumbers, PREFIX_PHONE);
-            PhoneContainsNumberPredicate phonePredicate = getPhonePredicate(phoneNumbers);
+            ParserUtil.checkEmptyString(phoneNumbers, PREFIX_PHONE);
+            PhoneContainsNumberPredicate phonePredicate = ParserUtil.getPhonePredicate(phoneNumbers, true);
             predicates.add(phonePredicate);
         }
         if (emailKeywords != null) {
-            checkEmptyString(emailKeywords, PREFIX_EMAIL);
-            EmailContainsKeywordsPredicate emailPredicate = getEmailPredicate(emailKeywords);
+            ParserUtil.checkEmptyString(emailKeywords, PREFIX_EMAIL);
+            EmailContainsKeywordsPredicate emailPredicate = ParserUtil.getEmailPredicate(emailKeywords, true);
             predicates.add(emailPredicate);
         }
         if (doneKeywords != null) {
-            checkEmptyString(doneKeywords, PREFIX_DONE);
-            checkTrueOrFalse(doneKeywords);
-            DonePredicate donePredicate = getDonePredicate(doneKeywords);
+            ParserUtil.checkEmptyString(doneKeywords, PREFIX_DONE);
+            ParserUtil.checkTrueOrFalse(doneKeywords);
+            DonePredicate donePredicate = ParserUtil.getDonePredicate(doneKeywords, true);
             predicates.add(donePredicate);
         }
         if (addressKeywords != null) {
-            checkEmptyString(addressKeywords, PREFIX_ADDRESS);
-            AddressContainsKeywordsPredicate addressPredicate = getAddressPredicate(addressKeywords);
+            ParserUtil.checkEmptyString(addressKeywords, PREFIX_ADDRESS);
+            AddressContainsKeywordsPredicate addressPredicate = ParserUtil.getAddressPredicate(addressKeywords, true);
             predicates.add(addressPredicate);
         }
         if (genderKeywords != null) {
-            checkEmptyString(genderKeywords, PREFIX_GENDER);
-            checkMaleOrFemale(genderKeywords);
-            GenderContainsKeywordPredicate genderPredicate = getGenderPredicate(genderKeywords);
+            ParserUtil.checkEmptyString(genderKeywords, PREFIX_GENDER);
+            ParserUtil.checkMaleOrFemale(genderKeywords);
+            GenderContainsKeywordPredicate genderPredicate = ParserUtil.getGenderPredicate(genderKeywords, true);
             predicates.add(genderPredicate);
         }
         if (ageValues != null) {
-            checkEmptyString(ageValues, PREFIX_AGE);
-            AgeContainsValuePredicate agePredicate = getAgePredicate(ageValues);
+            ParserUtil.checkEmptyString(ageValues, PREFIX_AGE);
+            AgeContainsValuePredicate agePredicate = ParserUtil.getAgePredicate(ageValues, true);
             predicates.add(agePredicate);
         }
         if (interestKeywords != null) {
-            checkEmptyString(interestKeywords, PREFIX_INTEREST);
-            InterestContainsKeywordsPredicate interestPredicate = getInterestPredicate(interestKeywords);
+            ParserUtil.checkEmptyString(interestKeywords, PREFIX_INTEREST);
+            InterestContainsKeywordsPredicate interestPredicate = ParserUtil.getInterestPredicate(interestKeywords, true);
             predicates.add(interestPredicate);
         }
 
@@ -111,106 +111,4 @@ public class FindAllCommandParser implements Parser<FindAllCommand> {
 
         return new FindAllCommand(combinedPredicate);
     }
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap) {
-        return Stream.of(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_DONE,
-                        PREFIX_ADDRESS, PREFIX_GENDER, PREFIX_AGE, PREFIX_INTEREST)
-                .anyMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
-
-    /**
-     * Checks if the string provided at the respective prefix is empty
-     * @param test The string to be checked
-     * @param prefix The prefix the string has
-     * @throws ParseException when the string is empty
-     */
-    private void checkEmptyString(String test, Prefix prefix) throws ParseException {
-        requireNonNull(test);
-        requireNonNull(prefix);
-        if (test.length() == 0) {
-            throw new ParseException(String.format(
-                    MESSAGE_INVALID_COMMAND_FORMAT, EMPTY_FIELD_MESSAGE + prefix.getPrefix()
-            ));
-        }
-    }
-
-    private NameContainsKeywordsPredicate getNamePredicate(String nameKeywords) {
-        String[] nameKeywordsArr = nameKeywords.split("\\s+");
-        return new NameContainsKeywordsPredicate(Arrays.asList(nameKeywordsArr), true);
-    }
-
-    private PhoneContainsNumberPredicate getPhonePredicate(String numbers) {
-        String[] numbersArr = numbers.split("\\s+");
-        return new PhoneContainsNumberPredicate(Arrays.asList(numbersArr), true);
-    }
-
-    private EmailContainsKeywordsPredicate getEmailPredicate(String emailKeywords) {
-        String[] emailKeywordsArr = emailKeywords.split("\\s+");
-        System.out.println("hello");
-        return new EmailContainsKeywordsPredicate(Arrays.asList(emailKeywordsArr), true);
-    }
-
-    private DonePredicate getDonePredicate(String doneKeywords) {
-        String[] doneKeywordsArr = doneKeywords.split("\\s+");
-        return new DonePredicate(Arrays.asList(doneKeywordsArr), true);
-    }
-
-    private AddressContainsKeywordsPredicate getAddressPredicate(String addressKeywords) {
-        String[] addressKeywordsArr = addressKeywords.split("\\s+");
-        return new AddressContainsKeywordsPredicate(Arrays.asList(addressKeywordsArr), true);
-    }
-
-    private GenderContainsKeywordPredicate getGenderPredicate(String genderKeywords) {
-        String[] genderKeywordsArr = genderKeywords.split("\\s+");
-        return new GenderContainsKeywordPredicate(Arrays.asList(genderKeywordsArr), true);
-    }
-
-    private AgeContainsValuePredicate getAgePredicate(String ageValues) {
-        String[] ageValuesArr = ageValues.split("\\s+");
-        return new AgeContainsValuePredicate(Arrays.asList(ageValuesArr), true);
-    }
-
-    private InterestContainsKeywordsPredicate getInterestPredicate(String interestKeywords) {
-        String[] interestKeywordsArr = interestKeywords.split("\\s+");
-        return new InterestContainsKeywordsPredicate(Arrays.asList(interestKeywordsArr), true);
-    }
-
-    /**
-     * Checks if the string supplied is either "t", "true", "f" or "false".(case-insensitive)
-     * @param s The String to be checked
-     * @return True if the string supplied is either "t", "true", "f" or "false". False otherwise
-     */
-    private void checkTrueOrFalse(String s) throws ParseException {
-        String[] testStrings = s.split("\\s+");
-
-        for (String test : testStrings) {
-            test = test.toLowerCase(Locale.ROOT);
-            if (!(test.equals("t") || test.equals("true")
-                    || test.equals("f") || test.equals("false"))) {
-                throw new ParseException("'d/' can only be followed by 't','f', 'true', or 'false'");
-            }
-        }
-    }
-
-    /**
-     * Checks if the string supplied is either "m", "male", "f", "female" or "n.a". (case-insensitive)
-     * @param s The String to be checked
-     * @return True if the string supplied is either "t", "true", "f" or "false". False otherwise
-     */
-    private void checkMaleOrFemale(String s) throws ParseException {
-        String[] testStrings = s.split("\\s+");
-
-        for (String test : testStrings) {
-            test = test.toLowerCase(Locale.ROOT);
-            if (!(test.equals("m") || test.equals("male")
-                    || test.equals("f") || test.equals("female") || test.equals("n.a"))) {
-                throw new ParseException("'g/' can only be followed by 'm','f', 'male', 'female' or 'N.A'");
-            }
-        }
-    }
-
 }
--- src/main/java/seedu/address/logic/parser/FindAnyCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindAnyCommandParser.java
@@ -17,6 +17,7 @@ import java.util.Locale;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+import seedu.address.logic.commands.FindAllCommand;
 import seedu.address.logic.commands.FindAnyCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.Person;
@@ -48,8 +49,8 @@ public class FindAnyCommandParser implements Parser<FindAnyCommand> {
                 ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_DONE,
                         PREFIX_ADDRESS, PREFIX_GENDER, PREFIX_AGE, PREFIX_INTEREST);
 
-        if (!arePrefixesPresent(argMultimap) || !argMultimap.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindAnyCommand.MESSAGE_USAGE));
+        if (!ParserUtil.arePrefixesPresent(argMultimap) || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindAllCommand.MESSAGE_USAGE));
         }
 
         String nameKeywords = argMultimap.getValue(PREFIX_NAME).orElse(null);
@@ -64,45 +65,45 @@ public class FindAnyCommandParser implements Parser<FindAnyCommand> {
         ArrayList<Predicate<Person>> predicates = new ArrayList<>();
 
         if (nameKeywords != null) {
-            checkEmptyString(nameKeywords, PREFIX_NAME);
-            NameContainsKeywordsPredicate namePredicate = getNamePredicate(nameKeywords);
+            ParserUtil.checkEmptyString(nameKeywords, PREFIX_NAME);
+            NameContainsKeywordsPredicate namePredicate = ParserUtil.getNamePredicate(nameKeywords, false);
             predicates.add(namePredicate);
         }
         if (phoneNumbers != null) {
-            checkEmptyString(phoneNumbers, PREFIX_PHONE);
-            PhoneContainsNumberPredicate phonePredicate = getPhonePredicate(phoneNumbers);
+            ParserUtil.checkEmptyString(phoneNumbers, PREFIX_PHONE);
+            PhoneContainsNumberPredicate phonePredicate = ParserUtil.getPhonePredicate(phoneNumbers, false);
             predicates.add(phonePredicate);
         }
         if (emailKeywords != null) {
-            checkEmptyString(emailKeywords, PREFIX_EMAIL);
-            EmailContainsKeywordsPredicate emailPredicate = getEmailPredicate(emailKeywords);
+            ParserUtil.checkEmptyString(emailKeywords, PREFIX_EMAIL);
+            EmailContainsKeywordsPredicate emailPredicate = ParserUtil.getEmailPredicate(emailKeywords, false);
             predicates.add(emailPredicate);
         }
         if (doneKeywords != null) {
-            checkEmptyString(doneKeywords, PREFIX_DONE);
-            checkTrueOrFalse(doneKeywords);
-            DonePredicate donePredicate = getDonePredicate(doneKeywords);
+            ParserUtil.checkEmptyString(doneKeywords, PREFIX_DONE);
+            ParserUtil.checkTrueOrFalse(doneKeywords);
+            DonePredicate donePredicate = ParserUtil.getDonePredicate(doneKeywords, false);
             predicates.add(donePredicate);
         }
         if (addressKeywords != null) {
-            checkEmptyString(addressKeywords, PREFIX_ADDRESS);
-            AddressContainsKeywordsPredicate addressPredicate = getAddressPredicate(addressKeywords);
+            ParserUtil.checkEmptyString(addressKeywords, PREFIX_ADDRESS);
+            AddressContainsKeywordsPredicate addressPredicate = ParserUtil.getAddressPredicate(addressKeywords, false);
             predicates.add(addressPredicate);
         }
         if (genderKeywords != null) {
-            checkEmptyString(genderKeywords, PREFIX_GENDER);
-            checkMaleOrFemale(genderKeywords);
-            GenderContainsKeywordPredicate genderPredicate = getGenderPredicate(genderKeywords);
+            ParserUtil.checkEmptyString(genderKeywords, PREFIX_GENDER);
+            ParserUtil.checkMaleOrFemale(genderKeywords);
+            GenderContainsKeywordPredicate genderPredicate = ParserUtil.getGenderPredicate(genderKeywords, false);
             predicates.add(genderPredicate);
         }
         if (ageValues != null) {
-            checkEmptyString(ageValues, PREFIX_AGE);
-            AgeContainsValuePredicate agePredicate = getAgePredicate(ageValues);
+            ParserUtil.checkEmptyString(ageValues, PREFIX_AGE);
+            AgeContainsValuePredicate agePredicate = ParserUtil.getAgePredicate(ageValues, false);
             predicates.add(agePredicate);
         }
         if (interestKeywords != null) {
-            checkEmptyString(interestKeywords, PREFIX_INTEREST);
-            InterestContainsKeywordsPredicate interestPredicate = getInterestPredicate(interestKeywords);
+            ParserUtil.checkEmptyString(interestKeywords, PREFIX_INTEREST);
+            InterestContainsKeywordsPredicate interestPredicate = ParserUtil.getInterestPredicate(interestKeywords, false);
             predicates.add(interestPredicate);
         }
 
@@ -110,105 +111,4 @@ public class FindAnyCommandParser implements Parser<FindAnyCommand> {
 
         return new FindAnyCommand(combinedPredicate);
     }
-
-    /**
-     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
-     * {@code ArgumentMultimap}.
-     */
-    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap) {
-        return Stream.of(PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_DONE,
-                PREFIX_ADDRESS, PREFIX_GENDER, PREFIX_AGE, PREFIX_INTEREST)
-                .anyMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
-    }
-
-    /**
-     * Checks if the string provided at the respective prefix is empty
-     * @param test The string to be checked
-     * @param prefix The prefix the string has
-     * @throws ParseException when the string is empty
-     */
-    private void checkEmptyString(String test, Prefix prefix) throws ParseException {
-        requireNonNull(test);
-        requireNonNull(prefix);
-        if (test.length() == 0) {
-            throw new ParseException(String.format(
-                    MESSAGE_INVALID_COMMAND_FORMAT, EMPTY_FIELD_MESSAGE + prefix.getPrefix()
-            ));
-        }
-    }
-
-    private NameContainsKeywordsPredicate getNamePredicate(String nameKeywords) {
-        String[] nameKeywordsArr = nameKeywords.split("\\s+");
-        return new NameContainsKeywordsPredicate(Arrays.asList(nameKeywordsArr), false);
-    }
-
-    private PhoneContainsNumberPredicate getPhonePredicate(String numbers) {
-        String[] numbersArr = numbers.split("\\s+");
-        return new PhoneContainsNumberPredicate(Arrays.asList(numbersArr), false);
-    }
-
-    private EmailContainsKeywordsPredicate getEmailPredicate(String emailKeywords) {
-        String[] emailKeywordsArr = emailKeywords.split("\\s+");
-        return new EmailContainsKeywordsPredicate(Arrays.asList(emailKeywordsArr), false);
-    }
-
-    private DonePredicate getDonePredicate(String doneKeywords) {
-        String[] doneKeywordsArr = doneKeywords.split("\\s+");
-        return new DonePredicate(Arrays.asList(doneKeywordsArr), false);
-    }
-
-    private AddressContainsKeywordsPredicate getAddressPredicate(String addressKeywords) {
-        String[] addressKeywordsArr = addressKeywords.split("\\s+");
-        return new AddressContainsKeywordsPredicate(Arrays.asList(addressKeywordsArr), false);
-    }
-
-    private GenderContainsKeywordPredicate getGenderPredicate(String genderKeywords) {
-        String[] genderKeywordsArr = genderKeywords.split("\\s+");
-        return new GenderContainsKeywordPredicate(Arrays.asList(genderKeywordsArr), false);
-    }
-
-    private AgeContainsValuePredicate getAgePredicate(String ageValues) {
-        String[] ageValuesArr = ageValues.split("\\s+");
-        return new AgeContainsValuePredicate(Arrays.asList(ageValuesArr), false);
-    }
-
-    private InterestContainsKeywordsPredicate getInterestPredicate(String interestKeywords) {
-        String[] interestKeywordsArr = interestKeywords.split("\\s+");
-        return new InterestContainsKeywordsPredicate(Arrays.asList(interestKeywordsArr), false);
-    }
-
-    /**
-     * Checks if the string supplied is either "t", "true", "f" or "false". (case-insensitive)
-     * @param s The String to be checked
-     * @return True if the string supplied is either "t", "true", "f" or "false". False otherwise
-     */
-    private void checkTrueOrFalse(String s) throws ParseException {
-        String[] testStrings = s.split("\\s+");
-
-        for (String test : testStrings) {
-            test = test.toLowerCase(Locale.ROOT);
-            if (!(test.equals("t") || test.equals("true")
-                    || test.equals("f") || test.equals("false"))) {
-                throw new ParseException("'d/' can only be followed by 't','f', 'true', or 'false'");
-            }
-        }
-    }
-
-    /**
-     * Checks if the string supplied is either "m", "male", "f", "female" or "n.a". (case-insensitive)
-     * @param s The String to be checked
-     * @return True if the string supplied is either "t", "true", "f" or "false". False otherwise
-     */
-    private void checkMaleOrFemale(String s) throws ParseException {
-        String[] testStrings = s.split("\\s+");
-
-        for (String test : testStrings) {
-            test = test.toLowerCase(Locale.ROOT);
-            if (!(test.equals("m") || test.equals("male")
-                    || test.equals("f") || test.equals("female") || test.equals("n.a"))) {
-                throw new ParseException("'g/' can only be followed by 'm','f', 'male', 'female' or 'N.A'");
-            }
-        }
-    }
-
 }
